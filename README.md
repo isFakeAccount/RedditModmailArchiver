7c874a5bfbe2f63ea2edf8839f0a696bd5294333
isFakeAccount <trevorphillips@gmx.us>
2021-08-13 00:32:50 -0400

    Added readme and reduced the code since marking the modmail read in archived is useless

---
+++ README.md
@@ -0,0 +1,42 @@
+
+# RedditModmailArchiver  
+  
+This bot will allow you to continuously archive the modmail by certain users. This is to prevent malicious users to clog your modmail.
+
+### How to use the bot
+1. First download this repository to your maching
+2.  Create a file called `praw.ini`. There you will provide the api keys and secret. 
+`Praw.ini` should look like this 
+```
+[modmail_archiver_bot]  
+client_id=Y4PJOclpDQy3xZ
+client_secret=UkGLTe6oqsMk5nHCJTHLrwgvHpr
+password=pni9ubeht4wd50gk
+username=fakebot1
+``` 
+(*Substitute the  example id, secret, username, and password values with actual values*)
+
+3. The next part is installing requirements from requirements.txt. This can be done via terminal using command
+```
+ pip3 install -r requirements.txt
+```
+4. Lastly run the main.py file
+
+### Config File
+The bot will look at the wiki page to gets its config information. In the wiki page you can mention the usernames of account which should be shadowbanned from the modmail. It's format should be
+```
+name:
+   - username1
+   - username2
+   - username3
+```
+Note: Do not add u\ before username. Also, the formating should be like above, otherwise bot will not work. Similar to Automoderator, the config is YAML. Learn more about [YAML Syntax](https://docs.ansible.com/ansible/latest/reference_appendices/YAMLSyntax.html)
+
+### FAQ
+**How it is better than muting?**
+For the most cases, you are better off muting user. However, in rare ocassions, if the user is bypassing the mute by creating multtiple accounts. This bot will come in handy. It is in a way modmail shadowban.
+
+**The modmail gets archived but it is still unread.**
+Unfortunately, this is not a fixable issue. The bot can move the modmail to archive and mark it read from its side. However, for other moderations, it will show as unread in archived box. A solution would be that each moderator has to go to archive box and select mark all read in top right corner. 
+
+[Archive messages showing unread](https://www.reddit.com/r/ModSupport/comments/oktfda/archive_messages_showing_unread/)
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import time
 from platform import platform
-from threading import Thread
 
 import praw
 import yaml
@@ -9,7 +8,7 @@ from prawcore.exceptions import PrawcoreException
 
 def load_config():
     config = None
-    wiki = subreddit_inst_1.wiki[wiki_page_path]
+    wiki = subreddit.wiki[wiki_page_path]
     try:
         config = yaml.safe_load(wiki.content_md)
     except yaml.YAMLError as yaml_err:
@@ -27,66 +26,31 @@ def archive_and_mark_as_read(modmail):
         modmail.read()
 
 
-def modmail_streamer():
-    modmail_stream = subreddit_inst_1.mod.stream.modmail_conversations(pause_after=-1, skip_existing=True)
-    while running:
-        try:
-            # Streams the new modmails
-            for modmail in modmail_stream:
-                if modmail is None:
-                    break
-                archive_and_mark_as_read(modmail)
-        except PrawcoreException as praw_exception:
-            print(praw_exception)
-            time.sleep(10)
-    exit(0)
-
-
-def archived_conversation_reader():
-    # # Once the modmail is archived. the steam won't be able to see it
-    # # So therefore this loop checks the archived modmails and marks them as read
+def main():
+    # Gets the recent modmails and checks the first author name against the list of banned users
+    # If the name matches the modmail gets archived.
     try:
-        for modmail in subreddit_inst_2.modmail.conversations(state='archived'):
+        for modmail in subreddit.modmail.conversations():
             archive_and_mark_as_read(modmail)
             time.sleep(5)
-
-            if not running:
-                exit(0)
     except PrawcoreException as praw_exception:
         print(praw_exception)
         time.sleep(10)
+    except KeyboardInterrupt:
+        print(time.strftime("The bot has stopped: %Y/%m/%d %I:%M:%S %r", time.localtime()))
 
 
 if __name__ == '__main__':
     # Logging in reddit
-    # PRAW is not thread safe, therefore we need two instances of praw
-    reddit1 = praw.Reddit('modmail_archiver_bot',
-                          user_agent=f"{platform()}:ModmailArchiver:1.0 (by /u/is_fake_Account)")
-    reddit2 = praw.Reddit('modmail_archiver_bot',
-                          user_agent=f"{platform()}:ModmailArchiver:1.0 (by /u/is_fake_Account)")
+    reddit = praw.Reddit('modmail_archiver_bot', user_agent=f"{platform()}:ModmailArchiver:1.0 (by /u/is_fake_Account)")
 
     subreddit_name = 'bramptonbois'
-    subreddit_inst_1 = reddit1.subreddit(subreddit_name)
-    subreddit_inst_2 = reddit2.subreddit(subreddit_name)
+    subreddit = reddit.subreddit(subreddit_name)
     wiki_page_path = 'modmail_archiver_config'
 
     # Printing status
-    print(f"Account Logged in {reddit1.user.me()}")
+    print(f"Account Logged in {reddit.user.me()}")
     print(f"Subreddit configured: r/{subreddit_name}")
     print(f"Config page name: {wiki_page_path}")
     print(time.strftime("Bot Started at: %Y/%m/%d %I:%M:%S %r", time.localtime()))
-
-    try:
-        # Running the bot
-        running = True
-        stream_thread = Thread(target=modmail_streamer)
-        archived_conv_thread = Thread(target=archived_conversation_reader)
-        stream_thread.start()
-        archived_conv_thread.start()
-        while True:
-            time.sleep(100)
-    except (KeyboardInterrupt, SystemExit):
-        running = False
-        print("Stopping the bot... This may take few seconds")
-
-    print(time.strftime("The bot has stopped: %Y/%m/%d %I:%M:%S %r", time.localtime()))
+    main()
